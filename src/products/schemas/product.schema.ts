f25be02760f6c7974da90415833c41b37db2ce04
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-19 18:56:26 +0900

    Feat : "추가 Product 스키마"

---
+++ src/products/schemas/product.schema.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema()
+export class Product {
+  @Prop()
+  title: string;
+
+  @Prop()
+  content: string;
+
+  @Prop()
+  img: string;
+
+  @Prop()
+  price: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
