8db0691a60c627f41689441a8b54f0dd05842359
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-20 15:26:50 +0900

    Feat : "추가 상품데이터 API"

---
+++ src/products/products.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductsService } from './products.service';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post('AddProduct')
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.createProduct(createProductDto);
+  }
+}
