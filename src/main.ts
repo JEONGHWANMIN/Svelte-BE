51aaf1489c3e15fde31527e283e452269ce410c1
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-20 17:56:22 +0900

    Feat : "추가 CORS 설정"

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // cors 설정
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
