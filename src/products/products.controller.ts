22ad7f38044db8a8aca13a27efaf23c7583247f1
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-20 17:56:43 +0900

    Feat : "추가 상품 조회 Controller"

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductsService } from './products.service';
+import { Product } from './schemas/product.schema';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post('AddProduct')
-  createProduct(@Body() createProductDto: CreateProductDto) {
+  async createProduct(@Body() createProductDto: CreateProductDto) {
     return this.productsService.createProduct(createProductDto);
+  }
+
+  @Get('')
+  async getProducts(): Promise<Product[]> {
+    return this.productsService.getProducts();
   }
 }
