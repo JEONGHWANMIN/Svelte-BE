eb51e23ae2cda4acea709accd25f910c1815cafd
JEONGHWANMIN <mimi0j0j@naver.com>
2022-06-20 17:58:01 +0900

    Feat : "상품 조회 서비스 로직 추가"

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { ProductsRepository } from './products.repository';
+import { Product } from './schemas/product.schema';
 
 @Injectable()
 export class ProductsService {
@@ -8,5 +9,9 @@ export class ProductsService {
 
   createProduct(createProductDto: CreateProductDto) {
     return this.productsRepository.create(createProductDto);
+  }
+
+  getProducts(): Promise<Product[]> {
+    return this.productsRepository.find({});
   }
 }
